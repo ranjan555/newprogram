89334f0fd4da07e6ad71a05fd24ff8662a7a678a
RANJAN DEVNATH <45716138+ranjan555@users.noreply.github.com>
2019-04-07 10:12:34 +0530

    A program for employee's salary  in c

---
+++ SALARY.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+double s,h,d,p,ns;
+clrscr();
+scanf("%lf",&s);
+h=s*5/100;
+d=s*10/100;
+p=s*12/100;
+printf("\nhra=%lf\nda=%lf\npf=%lf",h,d,p);
+ns=s+h+d-p;
+printf("\nTotal salary o my income=%lf",ns);
+getch();
+}
